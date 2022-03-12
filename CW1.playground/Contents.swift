6cf54659204adaca3b786f720a50a03ed8a15686
yaqoubalghadban <yoyob4654@gmail.com>
2022-03-12 18:16:49 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,16 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
+var name = "yaqoub"
+var bithdayYear : Int = 2004
+var age : Int
+
+age = 2022 - bithdayYear
+print("hello my name is \(name) and my age is \(age) ")
+
+var numb1 = "8"
+var numb2 = 4
+var numb3 = 2.5
+ Int(numb1)! + numb2
+numb3 = Double(Int(numb1)! + numb2)
 
